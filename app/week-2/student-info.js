3c3a7094365e2ff4eb81abccfb41496a688f9e27
Saheed lawal <166839975+SomoADE@users.noreply.github.com>
2025-01-28 17:25:01 -0700

    wwek-2 start

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,6 +2,15 @@ import Link from 'next/link';
 
 export default function page() {
     return(
-        
+        <div>
+        <h1>Your Name: [Saheed Adewale]</h1>
+        <p>
+          GitHub Repository:{' '}
+          <Link href="https://github.com/SomoADE">
+            Go to my GitHub
+          </Link>
+        </p>
+      </div>
+
     )
 }
