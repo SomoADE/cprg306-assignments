7c7d587d8a63b885526091f2a361760ceb9531ef
Saheed lawal <166839975+SomoADE@users.noreply.github.com>
2025-02-04 17:06:54 -0700

    week-3 start

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 const Page = () => {
     return (
         <main className="max-w-lg mx-auto p-6">
-            <h1 className="text-3xl  font-bold text-left  mb-3">Shopping List</h1>
+            <h1 className="text-3xl  bg-yellow-400 font-bold text-left  mb-3">Shopping List</h1>
             <ItemList />
         </main>
 
