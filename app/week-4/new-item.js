c2f0661ed65144d03f7a2f46c5c6af8b4e40376f
Saheed lawal <166839975+SomoADE@users.noreply.github.com>
2025-02-25 11:43:35 -0700

    week-5

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -17,18 +17,18 @@ export default function NewItem() {
     };
     return (
         <div className="flex flex-col items-center p-6 bg-yellow-600 rounded-lg shadow-md max-w-sm mx-auto">
-          <h2 className="text-xl font-bold mb-4 text-green-700">Select quantity</h2>
-          <div className="flex items-center space-x-4">
-            <button
-              onClick={decrement}
-              disabled={quantity === 1}
-              className="px-4 py-2 bg-red-600 text-white rounded-lg disabled:opacity-50"
-            > - </button>
-            <span className="text-lg font-semibold">{quantity}</span>
-            <button
-              onClick={increment}
-              disabled={quantity === 20}
-              className="px-4 py-2 bg-blue-600 text-white rounded-lg disabled:opacity-50"> + </button>
+            <h2 className="text-xl font-bold mb-4 text-green-700">Select quantity</h2>
+            <div className="flex items-center space-x-4">
+                <button
+                    onClick={decrement}
+                    disabled={quantity === 1}
+                    className="px-4 py-2 bg-red-600 text-white rounded-lg disabled:opacity-50"
+                > - </button>
+                <span className="text-lg font-semibold">{quantity}</span>
+                <button
+                    onClick={increment}
+                    disabled={quantity === 20}
+                    className="px-4 py-2 bg-blue-600 text-white rounded-lg disabled:opacity-50"> + </button>
             </div>
         </div>
     );
